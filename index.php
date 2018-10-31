b44fe37c49a588fafaeb4d52ccaeafc32cd4073b
Miguel Angel gutierrez <miguel.gutierrez@correounivalle.edu.co>
2018-10-31 13:13:04 -0500

    Primer repositorio

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Make It Real Test</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="webroot/img/make.png" type="image/x-icon" rel="shortcut icon">
+	<link rel="stylesheet" type="text/css" href="webroot/css/stylesheet.css">
+	<link rel="stylesheet" href="webroot/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<script src="webroot/js/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="webroot/js/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="webroot/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</head>
+<body>
+	<div class="row justify-content-center">
+		<div class="col-md-0 mt-5">
+			<div class="pt-5">
+				<img src="webroot/img/imagen-2.jpeg" alt="La imagen no se pudo cargar" class="rounded-circle customed-border" width="150" height="180">
+			</div>
+		</div>
+		<div class="col-md-12 mt-5 text-warning customed-text">
+			<strong><p class="font-italic">Miguel Angel Gutierrez Maya</p></strong>
+		</div>
+		<div class="col-md-12 mt-0 text-warning customed-text">
+			<strong><p class="font-italic">Palmira Valle, Col</p></strong>
+		</div>
+	</div>
+</body>
+</html>
